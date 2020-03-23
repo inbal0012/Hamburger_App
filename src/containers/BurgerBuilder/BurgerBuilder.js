c579c13a84045ff90a33e9ca85512a94fd1015c4
Inbal Schkolnik <capoeira0012>
2020-03-23 03:08:34 +0200

    Price added and showed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientsHandler}
           ingredientRemoved={this.removeIngredientsHandler}
           disabled={disabledInfo}
+          price={this.state.totelPrice}
         />
       </Aux>
     );
