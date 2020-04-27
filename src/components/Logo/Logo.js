b6865d87f9c9ee18b556f9ca6596840942911f3b
Inbal Schkolnik <capoeira0012>
2020-04-27 17:23:11 +0300

    SideDrawer added, Logo style added (used in Toolbar. in SideDrawer - using a different method (div and a local css class))

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,10 @@ import React from 'react';
 import BurgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
-const logo = props => (
-  <div className={classes.Logo}>
+const logo = (props) => (
+  <div className={classes.Logo} style={{ height: props.height }}>
+    {/*style - used in Toolbar
+        in SideDrawer - using a different method (div and a local css class) */}
     <img src={BurgerLogo} alt='MyBurger' />
   </div>
 );
