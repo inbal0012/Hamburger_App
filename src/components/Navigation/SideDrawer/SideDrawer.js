259ad6a16b3945b03665cd001c6166393730c1de
Inbal Schkolnik <capoeira0012>
2020-04-27 20:57:38 +0300

    Backdrop added to SideDrower layout became stateful component hamburger btn added DrawerToggle added

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,31 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/auxiliary';
+
 const sideDrawer = (props) => {
-  // ...
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  console.log(props.open);
+
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
+  console.log(attachedClasses);
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
