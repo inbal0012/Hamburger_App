af2fb7ae04328d5d32b1131d29dd122abc72d1ff
Inbal Schkolnik <capoeira0012>
2020-05-01 14:00:00 +0300

    Layout moved to HOC

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,13 +8,10 @@ import Aux from '../../../hoc/auxiliary';
 
 const sideDrawer = (props) => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
-  console.log(props.open);
 
   if (props.open) {
     attachedClasses = [classes.SideDrawer, classes.Open];
   }
-
-  console.log(attachedClasses);
 
   return (
     <Aux>
