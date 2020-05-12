5c57f18d4bec6d347dac17cdb4e63529fec2fcd2
Inbal Schkolnik <capoeira0012>
2020-05-12 17:47:32 +0300

    orders retrieved from server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then((res) => {
+        console.log(res.data);
+        let orders = [];
+        for (let key in res.data) {
+          orders.push({ ...res.data[key], id: key });
+        }
+        console.log(orders);
+        this.setState({ loading: false, orders });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map((order) => (
+          <Order
+            price={order.price}
+            ingredients={order.ingredients}
+            key={order.id}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
