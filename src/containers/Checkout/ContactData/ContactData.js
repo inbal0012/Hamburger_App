e88b1626486fbb97725b16d9f063d9f04430eb0e
Inbal Schkolnik <capoeira0012>
2020-05-13 14:04:25 +0300

    ContactData state changed, Input -  props names changed, select case added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,61 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zip: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
+          ],
+        },
+        value: '',
+      },
     },
     loading: false,
   };
@@ -25,16 +72,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: 'Inbal',
-        address: {
-          street: 'teststreet1',
-          zip: '158763',
-          country: 'Israel',
-        },
-        email: 'inbal0012@gmail.com',
-      },
-      deliveryMethod: 'fastest',
     };
 
     axios
@@ -50,36 +87,38 @@ class ContactData extends Component {
       });
   };
 
+  inputChangedHandler = (event, inputIdentefier) => {
+    const updatedOrderForm = { ...this.state.orderForm };
+    const updatedFormElement = { ...updatedOrderForm[inputIdentefier] };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentefier] = updatedFormElement;
+    this.setState({ orderForm: updatedOrderForm });
+  };
+
   render() {
+    const formElementArray = [];
+    for (let key in this.state.orderForm) {
+      formElementArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type='text'
-          name='name'
-          placeholder='Your name'
-        />
-        <input
-          className={classes.Input}
-          type='email'
-          name='email'
-          placeholder='Your email'
-        />
-        <input
-          className={classes.Input}
-          type='text'
-          name='street'
-          placeholder='Your street'
-        />
-        <input
-          className={classes.Input}
-          type='text'
-          name='postal'
-          placeholder='Your postalCode'
-        />
+        {formElementArray.map((
+          element //elementType: 'select',elementConfig
+        ) => (
+          <Input
+            key={element.id}
+            elementType={element.config.elementType}
+            elementConfig={element.config.elementConfig}
+            value={element.config.value}
+            changed={(event) => this.inputChangedHandler(event, element.id)}
+          />
+        ))}
         <Button btnType='Success' clicked={this.orderHandler}>
           ORDER
-        </Button>{' '}
+        </Button>
         {/*Todo add onClick*/}
       </form>
     );
